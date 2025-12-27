ff140f879d4a0ee549d6448a5b70b29367a8749e
Francisco Santos <1230564@isep.ipp.pt>
2025-12-27 01:41:10 +0000

    add email value object with @nodeDDD validation

---
+++ src/domain/Shared/email.ts
@@ -0,0 +1,37 @@
+import {ValueObject} from "../../core/domain/ValueObject";
+import {Guard} from "../../core/logic/Guard";
+import {Result} from "../../core/logic/Result";
+
+
+interface EmailProps {
+    value: string;
+}
+
+export class Email extends ValueObject<EmailProps> {
+    get value(): string {
+        return this.props.value;
+    }
+
+    private constructor(props: EmailProps) {
+        super(props);
+    }
+
+    private static isValidEmail(email: string): boolean {
+        const re = /^[^\s@]+@nodeDDD\.[^\s@.]+$/;
+        return re.test(email);
+    }
+
+    public static create(email: string): Result<Email> {
+        const guardResult = Guard.againstNullOrUndefined(email, 'email');
+
+        if (!guardResult.succeeded) {
+            return Result.fail<Email>(guardResult.message);
+        }
+
+        if (!this.isValidEmail(email)) {
+            return Result.fail<Email>(`The email "${email}" is invalid. All emails should belong to domain @nodeDDD`);
+        }
+
+        return Result.ok<Email>(new Email({ value: email.toLowerCase() }));
+    }
+}
