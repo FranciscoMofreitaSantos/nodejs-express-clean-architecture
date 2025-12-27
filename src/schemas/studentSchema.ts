f08352c468b118711c17b0778db4f8635808cac0
Francisco Santos <1230564@isep.ipp.pt>
2025-12-27 11:10:52 +0000

    implement student mongoose schema and persistence interface

---
+++ src/schemas/studentSchema.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import {IStudentPersistence} from "../persistence/schemas/studentSchema";
+
+const StudentSchema = new mongoose.Schema(
+    {
+
+        domainId: {
+            type: String,
+            unique: true,
+            required: true,
+            index: true
+        },
+
+        name: {
+            type: String,
+            required: true
+        },
+
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            index: true,
+            lowercase: true,
+            trim: true
+        },
+
+        classroomId: {
+            type: String,
+            required: false,
+            default: null
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+export default mongoose.model<IStudentPersistence & mongoose.Document>('Student', StudentSchema);
